9947118788f952db6e9bf691b60cfe63c788d512
Clara Fok <clarafok@google.com>
2025-02-24 16:54:36 -0800

    base commit

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+val buildId = "13113942"
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven {
+            // You can find the maven URL for other artifacts (e.g. KMP, METALAVA) on their
+            // build pages.
+            url = uri("https://androidx.dev/snapshots/builds/13113942/artifacts/repository")
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://androidx.dev/snapshots/builds/13113942/artifacts/repository")
+        }
+    }
+}
+
+rootProject.name = "BaseNav3DebugSetup"
+include(":app")
+ 
